2a7a24fece251f82efd6d50e718ed69199b4c9fb
bhadadi <bilelhadadi2208@gmail.com>
2024-03-22 16:22:19 +0100

    correction des bugs de test et implementation du nouveau ServiceJoueurImpl + ajout des test pseudo unique et de la langue incorrecte

--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/ServicesJoueurTest.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/ServicesJoueurTest.java
@@ -2,7 +2,6 @@ package org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.entites.dto.JoueurDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.*;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.impls.ServiceJoueurImpl;
@@ -13,77 +12,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 class ServicesJoueurTest {
-    IServicesJoueur servicesJoueur;
-    List<String> test = new ArrayList<>();
+
+//    IServicesJoueur servicesJoueur = new ServicesJoueursMockAjoutOK();
+    IServicesJoueur servicesJoueur = ServiceJoueurImpl.getInstance();
+    List<String> listeCentreInteret = new ArrayList<>();
 
     @Test
     public void testAddJoueur() throws AjoutJoueurException {
 //        servicesJoueur = new ServicesJoueursMockAjoutOK();
-        servicesJoueur = ServiceJoueurImpl.getInstance();
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 
-        JoueurDTO joueurAttendu = new JoueurDTO("emre","emrebondy",2015, "francais",test);
+        JoueurDTO joueurAttendu = new JoueurDTO("emre","emrebondys",2015, "francais", listeCentreInteret);
 
         //tout correct
-        JoueurDTO joueurResultat = servicesJoueur.creerJoueur("emre","emrebondy",2015,"francais",test);
+        JoueurDTO joueurResultat = servicesJoueur.creerJoueur("emre","emrebondys",2015,"francais", listeCentreInteret);
         Assertions.assertEquals(joueurAttendu,joueurResultat);
     }
     @Test
     public void testJoueurPrenomCarectereSpecial(){
         //prenom avec caractère spécial
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPrenonIncorrectMock();
         Assertions.assertThrows(PrenomIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre5", "emrebondy", 2015, "francais", test);
+            servicesJoueur.creerJoueur("emre5", "emrebondy1", 2015, "francais", listeCentreInteret);
         });
     }
     @Test
     public void testJoueurPrenomVide(){
         //prenom vide
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPrenonIncorrectMock();
         Assertions.assertThrows(PrenomIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("", "emrebondy", 2015, "francais", test);
+            servicesJoueur.creerJoueur("", "emrebondy2", 2015, "francais", listeCentreInteret);
         });
     }
     @Test
     public void testJoueurPrenomTailleSuperieur14(){
         //taille prenom supérieur ou égale à 15
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPrenonIncorrectMock();
         Assertions.assertThrows(PrenomIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emreemreemreemreemre", "emrebondy", 2015, "francais", test);
+            servicesJoueur.creerJoueur("emreemreemreemreemre", "emrebondy3", 2015, "francais", listeCentreInteret);
         });
     }
     @Test
     public void testJoueurPseudoTailleIncorrect(){
         //taille pseudo superieur à 19
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPseudoTailleIncorrectMock();
         Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emrebondy", "emrebondyemrebondyab", 2015, "francais", test);
+            servicesJoueur.creerJoueur("emrebondy", "emrebondyemrebondyab", 2015, "francais", listeCentreInteret);
         });
     }
     @Test
     public void testJoueurPseudoVide(){
         //pseudo vide
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPseudoTailleIncorrectMock();
         Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "", 2015, "francais", test);
+            servicesJoueur.creerJoueur("emre", "", 2015, "francais", listeCentreInteret);
         });
     }
     @Test
     public void testJoueurPseudoTropPetit(){
         //pseudo trop petit
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPseudoTailleIncorrectMock();
         Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "abc", 2015, "francais", test);
+            servicesJoueur.creerJoueur("emre", "abc", 2015, "francais", listeCentreInteret);
         });
     }
     @Test
-    public void testJoueurCentreInteretINcorrect(){
+    public void testJoueurCentreInteretIncorrect(){
         List<String> test2 = new ArrayList<>();
         test2.add("footballe");
         test2.add("basket");
@@ -91,34 +91,45 @@ class ServicesJoueurTest {
         test2.add("baseball");
 //        servicesJoueur = new ServiceJoueurCentreInteretIncorrectMock();
         Assertions.assertThrows(CentreInteretIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emrebondy", "emrebondyemrebondyab", 2015, "francais", test2);
+            servicesJoueur.creerJoueur("emrebondy", "emrebondy4", 2015, "francais", test2);
         });
     }
     @Test
-    public void testJoueurAnneeTropGrand(){
+    public void testJoueurAnneeTropPetite(){
         //date de naissance superieur à 2020
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurAnneeIncorrectMock();
         Assertions.assertThrows(AnneeIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "emrebondy", 1906, "francais", test);
+            servicesJoueur.creerJoueur("emre", "emrebondyr", 1906, "francais", listeCentreInteret);
         });
     }
     @Test
-    public void testJoueurAnneeTropPetit(){
+    public void testJoueurAnneeTropGrande(){
         //date de naissance incorect2
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurAnneeIncorrectMock();
         Assertions.assertThrows(AnneeIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "emrebondy", 2022,"francais", test);
+            servicesJoueur.creerJoueur("emre", "emrebondy6", 2022,"francais", listeCentreInteret);
         });
     }
     @Test
     public void testJoueurPseudoUnique() throws AjoutJoueurException {
         //pseudo deja existant
-        test.add("footballe");
+        listeCentreInteret.add("footballe");
 //        servicesJoueur = new ServiceJoueurPseudoExistantMock();
-        servicesJoueur.creerJoueur("emre","emreBondy",2004,"francais",test);
-        servicesJoueur.creerJoueur("bilel","emreBondy",2004,"francais",test);
+        servicesJoueur.creerJoueur("emre","emreBondy",2004,"francais", listeCentreInteret);
+        Assertions.assertThrows(PseudoExistantExeption.class, () -> {
+            servicesJoueur.creerJoueur("bilel","emreBondy",2004,"francais", listeCentreInteret);
+        });
+    }
+    @Test
+    public void testJoueurLangue() throws AjoutJoueurException {
+        //verifie que la langue soit correcte
+        listeCentreInteret.add("footballe");
+//        servicesJoueur = new ServiceJoueurPseudoExistantMock();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            servicesJoueur.creerJoueur("bilel","emreBondy",2004,"free", listeCentreInteret);
+        });
     }
 
 }
