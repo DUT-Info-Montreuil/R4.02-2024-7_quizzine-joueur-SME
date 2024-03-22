8ce519d2e8c8eb4a0d43226154991e204e1bc674
bhadadi <bilelhadadi2208@gmail.com>
2024-03-22 11:44:21 +0100

    implemation de l'ajout d'un joueur

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/service/impls/Main.java
@@ -0,0 +1,9 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.impls;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Tentative de conversion de chaîne en entier
+
+    }
+}
