e5746596ccc1721170817dc8db3d2d91c7245c07
emrebondy <gucluemre976@gmail.com>
2024-03-22 14:39:12 +0100

    correction des tests qui étaient en un block qui sont maintenant séparer en différente méthode

--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/ServicesJoueurTest.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/ServicesJoueurTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.entites.dto.JoueurDTO;
-import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.ServicesJoueursMockAjoutOK;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.*;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.impls.ServiceJoueurImpl;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.interfaces.IServicesJoueur;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.utiles.*;
@@ -13,95 +13,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 class ServicesJoueurTest {
-
     IServicesJoueur servicesJoueur;
+    List<String> test = new ArrayList<>();
+
     @Test
     public void testAddJoueur() throws AjoutJoueurException {
-        servicesJoueur = new ServiceJoueurImpl();
-//        servicesJoueur = new ServicesJoueursMockAjoutOK();
-
-        List<String> test = new ArrayList<>();
+        servicesJoueur = new ServicesJoueursMockAjoutOK();
         test.add("footballe");
+
         JoueurDTO joueurAttendu = new JoueurDTO("emre","emrebondy",2015, Langue.francais,test);
-//
-//        //tout correct
-//        public void testAddJoueurCorrect(){
-//
-//        }
+
+        //tout correct
         JoueurDTO joueurResultat = servicesJoueur.creerJoueur("emre","emrebondy",2015,Langue.francais,test);
         Assertions.assertEquals(joueurAttendu,joueurResultat);
-
+    }
+    @Test
+    public void testJoueurPrenomCarectereSpecial(){
         //prenom avec caractère spécial
-        //JoueurDTO joueurResultat2 = servicesJoueur.creerJoueur("emre5","emrebondy",2020,Langue.francais,test);
-        //Assertions.assertNotEquals(joueurAttendu, joueurResultat2);
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPrenonIncorrectMock();
         Assertions.assertThrows(PrenomIncorrectExeption.class, () -> {
             servicesJoueur.creerJoueur("emre5", "emrebondy", 2015, Langue.francais, test);
         });
-
-//        //date de naissance incorect
-//        JoueurDTO joueurResultat3 = servicesJoueur.creerJoueur("emre","emrebondy",1906,Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu, joueurResultat3);
-        Assertions.assertThrows(AnneeIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "emrebondy", 1906, Langue.francais, test);
-        });
-
-//
-//        //date de naissance incorect2
-//        JoueurDTO joueurResultat4 = servicesJoueur.creerJoueur("emre","emrebondy",2022,Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu, joueurResultat4);
-        Assertions.assertThrows(AnneeIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "emrebondy", 2022,Langue.francais, test);
-        });
-//
-//        //prenom vide
-//        JoueurDTO joueurResultat5 = servicesJoueur.creerJoueur("","emrebondy",2020,Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu, joueurResultat5);
+    }
+    @Test
+    public void testJoueurPrenomVide(){
+        //prenom vide
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPrenonIncorrectMock();
         Assertions.assertThrows(PrenomIncorrectExeption.class, () -> {
             servicesJoueur.creerJoueur("", "emrebondy", 2015, Langue.francais, test);
         });
-//
-//        //pseudo vide
-//        JoueurDTO joueurResultat6 = servicesJoueur.creerJoueur("emre","",2020,Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu, joueurResultat6);
-        Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "", 2015, Langue.francais, test);
-        });
-//
-//        //pseudo trop petit
-//        JoueurDTO joueurResultat9 = servicesJoueur.creerJoueur("emre","abc",2020,Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu, joueurResultat9);
-        Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
-            servicesJoueur.creerJoueur("emre", "abc", 2015, Langue.francais, test);
-        });
-//
-//
-//        JoueurDTO joueurAttendu2 = new JoueurDTO("emreemreemreem","emrebondyemrebondya",2020, Langue.francais,test);
-//
-//        //taille prenom supérieur ou égale à 15
-//        JoueurDTO joueurResultat7 = servicesJoueur.creerJoueur("emreemreemreemreemre","emrebondy",2020, Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu2, joueurResultat7);
+    }
+    @Test
+    public void testJoueurPrenomTailleSuperieur14(){
+        //taille prenom supérieur ou égale à 15
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPrenonIncorrectMock();
         Assertions.assertThrows(PrenomIncorrectExeption.class, () -> {
             servicesJoueur.creerJoueur("emreemreemreemreemre", "emrebondy", 2015, Langue.francais, test);
         });
-//
-//
-//        //taille pseudo supérieur ou égale à 20
-//        JoueurDTO joueurResultat8 = servicesJoueur.creerJoueur("emreemreemreem","emrebondyemrebondyab",2020, Langue.francais,test);
-//        Assertions.assertNotEquals(joueurAttendu2, joueurResultat8);
+    }
+    @Test
+    public void testJoueurPseudoTailleIncorrect(){
+        //taille pseudo superieur à 19
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPseudoTailleIncorrectMock();
         Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
             servicesJoueur.creerJoueur("emrebondy", "emrebondyemrebondyab", 2015, Langue.francais, test);
         });
-
+    }
+    @Test
+    public void testJoueurPseudoVide(){
+        //pseudo vide
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPseudoTailleIncorrectMock();
+        Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
+            servicesJoueur.creerJoueur("emre", "", 2015, Langue.francais, test);
+        });
+    }
+    @Test
+    public void testJoueurPseudoTropPetit(){
+        //pseudo trop petit
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPseudoTailleIncorrectMock();
+        Assertions.assertThrows(PseudoTailleIncorrectExeption.class, () -> {
+            servicesJoueur.creerJoueur("emre", "abc", 2015, Langue.francais, test);
+        });
+    }
+    @Test
+    public void testJoueurCentreInteretINcorrect(){
         List<String> test2 = new ArrayList<>();
         test2.add("footballe");
         test2.add("basket");
         test2.add("tennis");
         test2.add("baseball");
+        servicesJoueur = new ServiceJoueurCentreInteretIncorrectMock();
         Assertions.assertThrows(CentreInteretIncorrectExeption.class, () -> {
             servicesJoueur.creerJoueur("emrebondy", "emrebondyemrebondyab", 2015, Langue.francais, test2);
         });
-
     }
-
+    @Test
+    public void testJoueurAnneeTropGrand(){
+        //date de naissance superieur à 2020
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurAnneeIncorrectMock();
+        Assertions.assertThrows(AnneeIncorrectExeption.class, () -> {
+            servicesJoueur.creerJoueur("emre", "emrebondy", 1906, Langue.francais, test);
+        });
+    }
+    @Test
+    public void testJoueurAnneeTropPetit(){
+        //date de naissance incorect2
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurAnneeIncorrectMock();
+        Assertions.assertThrows(AnneeIncorrectExeption.class, () -> {
+            servicesJoueur.creerJoueur("emre", "emrebondy", 2022,Langue.francais, test);
+        });
+    }
+    @Test
+    public void testJoueurPseudoUnique() throws AjoutJoueurException {
+        //pseudo deja existant
+        test.add("footballe");
+        servicesJoueur = new ServiceJoueurPseudoExistantMock();
+        servicesJoueur.creerJoueur("emre","emreBondy",2004,Langue.francais,test);
+        servicesJoueur.creerJoueur("bilel","emreBondy",2004,Langue.francais,test);
+    }
 
 }
---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr7/quizzine/service/ServiceJoueurCentreInteretIncorrectMock.java
@@ -0,0 +1,16 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service;
+
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.entites.dto.JoueurDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.service.interfaces.IServicesJoueur;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.utiles.AjoutJoueurException;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.utiles.CentreInteretIncorrectExeption;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr7.quizzine.utiles.Langue;
+
+import java.util.List;
+
+public class ServiceJoueurCentreInteretIncorrectMock implements IServicesJoueur {
+    @Override
+    public JoueurDTO creerJoueur(String prenom, String pseudo, int anneeDeNaissance, Langue langue, List<String> centreInteret) throws AjoutJoueurException {
+        throw new CentreInteretIncorrectExeption();
+    }
+}
